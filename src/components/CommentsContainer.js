a3de5861a4d8b32273066c43aee7b9a14998bd99
Arshdeep Arora <arsharora017@gmail.com>
2024-06-03 14:58:58 -0400

    create comments section for videos

--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -1,9 +1,91 @@
 import React from "react";
+import Comment from "./Comment";
+import CommentsList from "./CommentsList";
+
+export const commentsData = [
+  {
+    name: "Arshdeep",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "Arshdeep",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [
+      {
+        name: "Arshdeep",
+        text: "Lorem ipsum dolor sit amet, consectetur adip",
+        replies: [
+          {
+            name: "Arshdeep",
+            text: "Lorem ipsum dolor sit amet, consectetur adip",
+            replies: [],
+          },
+          {
+            name: "Arshdeep",
+            text: "Lorem ipsum dolor sit amet, consectetur adip",
+            replies: [
+              {
+                name: "Arshdeep",
+                text: "Lorem ipsum dolor sit amet, consectetur adip",
+                replies: [
+                  {
+                    name: "Arshdeep",
+                    text: "Lorem ipsum dolor sit amet, consectetur adip",
+                    replies: [
+                      {
+                        name: "Arshdeep",
+                        text: "Lorem ipsum dolor sit amet, consectetur adip",
+                        replies: [],
+                      },
+                      {
+                        name: "Arshdeep",
+                        text: "Lorem ipsum dolor sit amet, consectetur adip",
+                        replies: [],
+                      },
+                    ],
+                  },
+                  {
+                    name: "Arshdeep",
+                    text: "Lorem ipsum dolor sit amet, consectetur adip",
+                    replies: [],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Arshdeep",
+        text: "Lorem ipsum dolor sit amet, consectetur adip",
+        replies: [],
+      },
+    ],
+  },
+  {
+    name: "Arshdeep",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "Arshdeep",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "Arshdeep",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+];
 
 const CommentsContainer = () => {
   return (
     <div className="m-5 p-2">
-      <h1>Comments: </h1>
+      <h1 className=" font-2xl font-bold">Comments: </h1>
+      {/* <Comment data={commentsData[0]} /> */}
+      <CommentsList comments={commentsData} />
     </div>
   );
 };
---
+++ src/components/CommentsList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Comment from "./Comment";
+
+const CommentsList = ({ comments }) => {
+  //Disclaimer: don't use indexes as keys
+  return comments.map((comment, index) => (
+    <div key={index}>
+      <Comment data={comment} />
+      <div className="pl-5 border border-l-black ml-5">
+        {/* recursing the component */}
+        <CommentsList comments={comment.replies} />
+      </div>
+    </div>
+  ));
+};
+
+export default CommentsList;
